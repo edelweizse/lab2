1b1c2f1d999f223d3b80f110ee67450331514cd8
edelweizse <mrmilkor@protonmail.com>
2024-10-16 19:47:46 +0500

    implemented user model

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ app.use(cors())
 
 app.use('/', authRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   connectDB()
   console.log(`Server is running on port ${PORT}`);
